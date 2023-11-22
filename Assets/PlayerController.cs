4a56bd94a3b42596285fe9b2890722a157d0eaff
JakubBerko <93920505+JakubBerko@users.noreply.github.com>
2023-11-22 11:02:43 +0100

    added vectors

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Vector2 primaryBallPos;
+    public Vector2 secondaryBallPos;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
