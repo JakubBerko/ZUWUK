c7d48516178a1f4b0e3aa2d86ef5be1a135af110
JakubBerko <93920505+JakubBerko@users.noreply.github.com>
2023-11-23 23:36:03 +0100

    player changes
    +player can now rotate
    +player can shoot balls with no functionality
    +player changes balls, but with no effect

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,19 +1,51 @@
+using DG.Tweening;
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public Vector2 primaryBallPos;
-    public Vector2 secondaryBallPos;
+    public Transform primaryBallPos;
+    public Transform secondaryBallPos;
+    public GameObject ball1Prefab;
+    public GameObject ball2Prefab;
+
+    private GameObject primBall;
+    private GameObject secBall;
     void Start()
     {
-        
+        primBall = Instantiate(ball1Prefab, primaryBallPos.position, Quaternion.identity);
+        primBall.transform.parent = transform;
+        secBall = Instantiate(ball2Prefab, secondaryBallPos.position, Quaternion.identity);
+        secBall.transform.parent = transform;
+        //nastavit scale (bud mala nebo velka, podle primary nebo secondary)
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        RotatePlayer();
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            primBall.transform.DOMove(mousePosition, 1f)
+                .SetEase(Ease.Linear);
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            secBall.transform.position = primaryBallPos.position;
+            secBall = primBall;
+            primBall.transform.position = secondaryBallPos.position;
+            primBall = secBall;
+        }
+    }
+
+    private void RotatePlayer()
+    {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 direction = mousePosition - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
     }
 }
